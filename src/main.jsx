c64029a1f5aa9d293d6723ab9a6d29e7ba9c6096
HutchAlpha <qboisset@hotmail.com>
2026-05-27 13:23:48 +0200

    Amélioration rangement page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/main.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import store from './redux/store.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <Header />
+      <StrictMode>
+        <App />
+      </StrictMode>
+    <Footer />
+  </Provider>
 )
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-const User = require('../database/models/userModel')
 
 const BankSlice = createSlice({
     name: "bank",
