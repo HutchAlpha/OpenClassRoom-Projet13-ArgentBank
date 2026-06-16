8f620cb6a6641f5ffdc3203ef05557b16f0158b7
HutchAlpha <qboisset@hotmail.com>
2026-06-16 12:12:38 +0200

    Ajout d'un message personnalisé pour les utilisateur qui n'arrive pas a se connecter

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -8,14 +8,17 @@ function SignInPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [rememberMe, setRememberMe] = useState(false)
+  const [error, setError] = useState('')
 
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    setError('')
 
-    //! Connexion + Récupération du token JWT
+    try {
+      //! Connexion + Récupération du token JWT
       const response = await axios.post(
         'http://localhost:3001/api/v1/user/login',
         {
@@ -28,71 +31,76 @@ function SignInPage() {
 
       localStorage.setItem('token', token)
 
-      //!Envoie a Redux 
+      //! Envoi à Redux
       dispatch(GestionToken(token))
       dispatch(GestionConnexion(true))
 
       //! Récupération des données de l'utilisateur
       const donneesUser = await axios.post(
-        'http://localhost:3001/api/v1/user/profile', 
+        'http://localhost:3001/api/v1/user/profile',
         {},
         {
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        }) 
-            
-      //!Envoie a Redux 
+        }
+      )
+
+      //! Envoi à Redux
       dispatch(GestionUser(donneesUser.data.body))
 
       navigate('/user')
-
+    } catch (err) {
+      setError('Incorrect username or password')
+      console.error(err)
+    }
   }
 
   return (
-    <>
-      <main className="main bg-dark">
-        <section className="sign-in-content">
-          <i className="fa fa-user-circle sign-in-icon"></i>
-          <h1>Sign In</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="input-wrapper">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
 
-            <div className="input-wrapper">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
+        {error && <p className="error-message">{error}</p>}
 
-            <div className="input-remember">
-              <input
-                type="checkbox"
-                id="remember-me"
-                checked={rememberMe}
-                onChange={(e) => setRememberMe(e.target.checked)}
-              />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
+        <form onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
 
-            <button type="submit" className="sign-in-button">
-              Sign In
-            </button>
-          </form>
-        </section>
-      </main>
-    </>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="input-remember">
+            <input
+              type="checkbox"
+              id="remember-me"
+              checked={rememberMe}
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+
+          <button type="submit" className="sign-in-button">
+            Sign In
+          </button>
+        </form>
+      </section>
+    </main>
   )
 }
 
